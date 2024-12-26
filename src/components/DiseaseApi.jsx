f2aa0574e3b0fcfe938902d48192805a48f9afec
Shaik Sonia <shaiksonia222@gmail.com>
2024-12-26 23:01:26 +0530

    update

--- src/components/DiseaseApi.jsx
+++ src/components/DiseaseApi.jsx
@@ -12,7 +12,7 @@ const DiseaseApi = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setDebouncedQuery(query);
-    }, 300); // Adjust the time (500ms) based on your preference
+    }, 300); 
 
     return () => {
       clearTimeout(timer); // Clear the timer if the query changes before the timeout
@@ -65,17 +65,21 @@ const DiseaseApi = () => {
 
           {/* Search bar inside the "Exploring the Path to Disease Targets" section */}
           <div className="flex justify-center mb-6">
-          <form onSubmit={(e) => e.preventDefault()} className="rounded-lg">
-  <input
-    type="text"
-    value={query}
-    onChange={(e) => setQuery(e.target.value)}
-    placeholder="Search for disease (e.g., cancer)"
-    className="border-2 border-gray-300 p-3 rounded-full w-80 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500  transition transform hover:translate-y-[-4px] hover:shadow-lg ease-in-out"
-  />
-</form>
+  <form onSubmit={(e) => e.preventDefault()} className="relative w-[50vw]">
+    <i
+      className="fas fa-search absolute top-1/2 left-4 text-gray-500 text-lg transform -translate-y-1/2"
+      aria-hidden="true"
+    ></i>
+    <input
+      type="text"
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      placeholder="Search for disease (e.g., cancer)"
+      className="border-2 border-gray-800 p-3 pl-12 rounded-full w-full text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 transition transform hover:translate-y-[-4px] hover:shadow-lg ease-in-out"
+    />
+  </form>
+</div>
 
-          </div>
 
           {/* If no query is provided, display featured diseases */}
           {!query && (
